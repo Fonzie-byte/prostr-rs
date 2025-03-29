9f0ca5b1ae7f72b765f99c32acb267c17bcf102d
Fonzie-byte <twistfons@live.nl>
2025-03-29 22:08:52 +0100

    Add the files because ofc I wroite everything before committing #git-hygiene

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use rand::prelude::IndexedRandom;
+use std::env;
+
+const VOWELS: [char; 5] = ['a', 'e', 'i', 'o', 'u'];
+const CONSONANTS: [char; 20] = [
+    'b', 'c', 'd', 'f', 'g',
+    'h', 'j', 'k', 'l', 'm',
+    'n', 'p', 'q', 'r', 's',
+    't', 'v', 'w', 'x', 'z',
+];
+
+fn main() {
+    let mut rng = rand::rng();
+    let length: u32 = env::args()
+        .nth(1)
+        .unwrap_or("8".to_string())
+        .parse()
+        .unwrap_or(8);
+
+    for i in 0..length {
+        if i % 2 == 0 {
+            print!("{}", CONSONANTS.choose(&mut rng).unwrap());
+        } else {
+            print!("{}", VOWELS.choose(&mut rng).unwrap());
+        };
+    }
+    println!();
+}
